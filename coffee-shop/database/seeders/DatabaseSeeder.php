64dca8739b3d7d6be53b5ad64482f11aece2d1cd
ngoinhaoto <dinhchinhle487@gmail.com>
2024-12-10 10:51:19 +0700

    LAB1 - DONE

---
+++ coffee-shop/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Database\Seeders\CategorySeeder;
+use Database\Seeders\ProductSeeder;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            CategorySeeder::class,
+            ProductSeeder::class,
+        ]);
+    }
+}
