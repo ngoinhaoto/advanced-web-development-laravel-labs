cf963c865e5678b11e08f02759b03c577a199b1c
ngoinhaoto <dinhchinhle487@gmail.com>
2024-12-19 21:24:45 +0700

    fixing unit test

--- coffee-shop/database/factories/CategoryFactory.php
+++ coffee-shop/database/factories/CategoryFactory.php
@@ -18,7 +18,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => fake()->sentence(),
-            'description' => fake()->paragraph(),
+            'description' => fake()->sentence(),
         ];
     }
 }
