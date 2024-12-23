2a353d36bb17fd5151a2c15d4ab2cfa51ca567b2
ngoinhaoto <dinhchinhle487@gmail.com>
2024-12-24 00:36:48 +0700

    fixing language not changing

--- coffee-shop/resources/views/livewire/setting-dropdown.blade.php
+++ coffee-shop/resources/views/livewire/setting-dropdown.blade.php
@@ -20,9 +20,9 @@
             focusAfter && focusAfter.focus()
         },
         setLocale(locale) {
-            $wire.setLocale(locale)
-    
-            location.reload()
+            $wire.setLocale(locale).then(() => {
+                location.reload();
+            });
         },
         setTheme(targetTheme) {
             $wire.setTheme(targetTheme)
